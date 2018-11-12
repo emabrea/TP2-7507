ce0195d7b4a970bb8f6b37797dd101bdb42c14ee
Santiago <santiagomariani2@gmail.com>
2018-11-11 21:29:54 -0300

    Ahi agrego lo que hice yo, tiene un diagrama y el codigo

--- Pruebas y codigo minimo/src/Modelo/Mapa.java
+++ Pruebas y codigo minimo/src/Modelo/Mapa.java
@@ -4,27 +4,13 @@ import java.util.ArrayList;
 
 public class Mapa {
 	
-	int altura;
-	int base;
+	int altura = 25s;
+	int base = 50;
 	ArrayList<Celda> celdas;
 	
-	public Mapa(int altura, int base){
-		this.altura = altura;
-		this.base = base;
+	public Mapa(){
 		celdas = new ArrayList<Celda>();
 		
-	}
-	
-	public static Mapa crearMapaAncho() {
-		int altura = 50;
-		int base = 100;
-		return new Mapa(altura, base);
-	}
-
-	public static Mapa crearMapaAlto(){
-		int altura = 100;
-		int base = 50;
-		return new Mapa(altura, base);
 	}
 
 	public void insertar(Insertable unObjeto, Posicion unaPosicion) {
