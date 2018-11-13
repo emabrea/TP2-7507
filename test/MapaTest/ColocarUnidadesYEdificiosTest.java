54f8aa0f32560f05e9f4e14b61731979a96327c7
Emanuel <42225702+emabrea@users.noreply.github.com>
2018-11-12 23:13:00 -0300

    Add files via upload
    Actualizo pruebas mapa

--- test/MapaTest/ColocarUnidadesYEdificiosTest.java
+++ test/MapaTest/ColocarUnidadesYEdificiosTest.java
@@ -3,43 +3,39 @@ package MapaTest;
 import org.junit.Assert;
 import org.junit.Test;
 
-import Modelo.Aldeano;
-import Modelo.Castillo;
-import Modelo.Cuartel;
+import Modelo.Posicion;
 import Modelo.Mapa;
+import Modelo.Cuartel;
 import Modelo.PlazaCentral;
-import Modelo.PosicionEdificio;
-import Modelo.PosicionUnidad;
+import Modelo.Castillo;
 
 public class ColocarUnidadesYEdificiosTest {
 
 	@Test
 	public void test01ColocarUnidadEnMapaVacioEnPosicionValida(){
-		Mapa mapa = new Mapa();
+		Mapa mapa = new Mapa(100,100);
 		
-		PosicionUnidad posicion = new PosicionUnidad(5, 6);
-		
-		mapa.insertar(new Aldeano(), posicion);
+		Posicion posicion = new Posicion(5, 6);
+
+		Aldeano aldeano = new Aldeano(posicion);		
 		
 		Assert.assertTrue(mapa.posicionOcupada(posicion));
 		
 	}
 	
 	@Test
-	public void test03ColocarCuartelEnPosicionValida(){
-		Mapa mapa = new Mapa();
+	public void test02ColocarCuartelEnPosicionValida(){
+		Mapa mapa = new Mapa(75,75);
+
+		Posicion posicion1 = new Posicion(10, 10);
+
+		Cuartel cuartel = new Cuartel(posicion1);	
 		
-		Cuartel cuartel = new Cuartel();
-		PosicionEdificio posicion = new PosicionEdificio(5, 6, cuartel.getTamanio());
+
+		Posicion posicion2 = new Posicion(11, 10);
+		Posicion posicion3 = new Posicion(10, 11);
+		Posicion posicion4 = new Posicion(11, 11);	
 		
-		PosicionUnidad posicion1= new PosicionUnidad(5, 6);
-		PosicionUnidad posicion2= new PosicionUnidad(5, 7);
-		PosicionUnidad posicion3= new PosicionUnidad(6, 6);
-		PosicionUnidad posicion4= new PosicionUnidad(6, 7);
-		
-		mapa.insertar(cuartel, posicion);
-		
-		Assert.assertTrue(mapa.posicionOcupada(posicion));
 		
 		Assert.assertTrue(mapa.posicionOcupada(posicion1));
 		Assert.assertTrue(mapa.posicionOcupada(posicion2));
@@ -49,72 +45,41 @@ public class ColocarUnidadesYEdificiosTest {
 
 	
 	@Test
-	public void test05ColocarPlazaCentralEnMapaVacioEnPosicionValida(){
-		Mapa mapa = new Mapa();
+	public void test03ColocarPlazaCentralEnMapaVacioEnPosicionValida(){
+		Mapa mapa = new Mapa(50,50);
+
+		Posicion posicion1 = new Posicion(20, 20);
+
+		PlazaCentral plaza = new PlazaCentral(posicion1);			
 		
-		PlazaCentral plazaCentral = new PlazaCentral();
-		PosicionEdificio posicion = new PosicionEdificio(5, 6, plazaCentral.getTamanio());
+
+		Posicion posicion2 = new Posicion(21, 20);
+		Posicion posicion3 = new Posicion(20, 21);
+		Posicion posicion4 = new Posicion(21, 21);	
 		
-		PosicionUnidad posicion1= new PosicionUnidad(5, 6);
-		PosicionUnidad posicion2= new PosicionUnidad(5, 7);
-		PosicionUnidad posicion3= new PosicionUnidad(6, 6);
-		PosicionUnidad posicion4= new PosicionUnidad(6, 7);
-		
-		mapa.insertar(plazaCentral, posicion);
-		
-		Assert.assertTrue(mapa.posicionOcupada(posicion));
 		
 		Assert.assertTrue(mapa.posicionOcupada(posicion1));
 		Assert.assertTrue(mapa.posicionOcupada(posicion2));
 		Assert.assertTrue(mapa.posicionOcupada(posicion3));
-		Assert.assertTrue(mapa.posicionOcupada(posicion4));			
+		Assert.assertTrue(mapa.posicionOcupada(posicion4));				
 	}
 
 	
 	@Test
-	public void test07ColocarCastilloEnMapaAnchoYVacioEnPosicionValida(){
-		Mapa mapa = Mapa.crearMapaAncho();
+	public void test04ColocarCastilloEnMapaAnchoYVacioEnPosicionValida(){
+		Mapa mapa = Mapa.crearMapaAncho(40,40);
+
+		Posicion posicionInicial = new Posicion(5, 5);
+		Castillo castillo = new Castillo(posicionInicial);
 		
-		Castillo castillo = new Castillo();
-		PosicionEdificio posicion = new PosicionEdificio(0, 1, castillo.getTamanio());
+
+		for(int i=5; i<5+4; i++){
+			for(int j=5; j<5+4 ; j++){
+				Posicion posicion = new Posicion(i, j);
+				Assert.assertTrue(mapa.posicionOcupada(posicion));
+			}
+		}	
 		
-		PosicionUnidad posicion1= new PosicionUnidad(0, 1);
-		PosicionUnidad posicion2= new PosicionUnidad(0, 2);
-		PosicionUnidad posicion3= new PosicionUnidad(0, 3);
-		PosicionUnidad posicion4= new PosicionUnidad(0, 4);
-		PosicionUnidad posicion5= new PosicionUnidad(1, 1);
-		PosicionUnidad posicion6= new PosicionUnidad(1, 2);
-		PosicionUnidad posicion7= new PosicionUnidad(1, 3);
-		PosicionUnidad posicion8= new PosicionUnidad(1, 4);
-		PosicionUnidad posicion9= new PosicionUnidad(2, 1);
-		PosicionUnidad posicion10= new PosicionUnidad(2, 2);
-		PosicionUnidad posicion11= new PosicionUnidad(2, 3);
-		PosicionUnidad posicion12= new PosicionUnidad(2, 4);
-		PosicionUnidad posicion13= new PosicionUnidad(3, 1);
-		PosicionUnidad posicion14= new PosicionUnidad(3, 2);
-		PosicionUnidad posicion15= new PosicionUnidad(3, 3);
-		PosicionUnidad posicion16= new PosicionUnidad(3, 4);
-		
-		mapa.insertar(castillo, posicion);
-		
-		Assert.assertTrue(mapa.posicionOcupada(posicion));
-		
-		Assert.assertTrue(mapa.posicionOcupada(posicion1));
-		Assert.assertTrue(mapa.posicionOcupada(posicion2));
-		Assert.assertTrue(mapa.posicionOcupada(posicion3));
-		Assert.assertTrue(mapa.posicionOcupada(posicion4));
-		Assert.assertTrue(mapa.posicionOcupada(posicion5));
-		Assert.assertTrue(mapa.posicionOcupada(posicion6));
-		Assert.assertTrue(mapa.posicionOcupada(posicion7));
-		Assert.assertTrue(mapa.posicionOcupada(posicion8));
-		Assert.assertTrue(mapa.posicionOcupada(posicion9));
-		Assert.assertTrue(mapa.posicionOcupada(posicion10));
-		Assert.assertTrue(mapa.posicionOcupada(posicion11));
-		Assert.assertTrue(mapa.posicionOcupada(posicion12));
-		Assert.assertTrue(mapa.posicionOcupada(posicion13));
-		Assert.assertTrue(mapa.posicionOcupada(posicion14));
-		Assert.assertTrue(mapa.posicionOcupada(posicion15));
-		Assert.assertTrue(mapa.posicionOcupada(posicion16));
 	}
 
 }
--- test/MapaTest/TamanioTest.java
+++ test/MapaTest/TamanioTest.java
@@ -7,16 +7,15 @@ import Modelo.Mapa;
 public class TamanioTest {
 
 		@Test
-		public void test01MapaMasAnchoQueAlto(){
+		public void test01LimitesMapa(){				
+			Mapa mapa = new Mapa(100,100);
+
+			Assert.assertTrue(mapa.casillaFueraDeRango(150,80));
+			Assert.assertTrue(mapa.casillaFueraDeRango(0,80));
+			Assert.assertTrue(mapa.casillaFueraDeRango(-8,50));
+			Assert.assertTrue(mapa.casillaFueraDeRango(101,100));
+			Assert.assertFalse(mapa.casillaFueraDeRango(100,100));
+			Assert.assertFalse(mapa.casillaFueraDeRango(50,50));
+		}	
 				
-			Mapa mapa = Mapa.crearMapaAncho();
-		}
-		
-		@Test
-		public void test02MapaMasAltoQueAncho(){
-			
-			Mapa mapa = Mapa.crearMapaAlto();
-		}
-		
-		
 }
