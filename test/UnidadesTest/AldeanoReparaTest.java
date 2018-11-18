53e0cd4310ecfc029166955fc56bca88003d4970
Emanuel <42225702+emabrea@users.noreply.github.com>
2018-11-18 18:03:12 -0300

    subo aldeano repara test

--- test/UnidadesTest/AldeanoReparaTest.java
+++ test/UnidadesTest/AldeanoReparaTest.java
@@ -1,5 +1,89 @@
 package UnidadesTest;
 
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+
+import Modelo.Aldeano;
+import Modelo.Celda;
+import Modelo.Mapa;
+import Modelo.Zona;
+import Modelo.Jugador;
+import Modelo.Edificio;
+
 public class AldeanoReparaTest {
+
+	@Before
+	public void reset(){
+		Mapa.reset();
+	}
+
+	@Test
+	public void test01AldeanoReparaCuartel(){		
+		
+		Celda celda = new Celda(15, 15);
+		Jugador jugador1 = new Jugador(100);
+
+		Aldeano aldeano = new Aldeano(celda,jugador1);
+		Mapa.obtenerInstancia().insertar(celda);
+		ArrayList<Zona> zonasPosibles = aldeano.posiblesZonasAConstruirCuartel();
+		Zona zonaAConstruir = zonasPosibles.get(0);
+		aldeano.construirCuartelEnZona(zonaAConstruir);
+
+		aldeano.realizarTareas();
+		aldeano.realizarTareas();
+		aldeano.realizarTareas();
+
+		Assert.assertTrue(aldeano.Libre());
+
+		Edificio cuartel = jugador1.obtenerEdificio();
+
+		Assert.assertTrue(cuartel.vidaActual()==250);
+		cuartel.recibirDanio(100);
+		Assert.assertTrue(cuartel.vidaActual()==150);
+		aldeano.repararEdificio(cuartel);
+		aldeano.realizarTareas();
+		Assert.assertTrue(cuartel.vidaActual()==200);
+		aldeano.realizarTareas();
+		Assert.assertTrue(cuartel.vidaActual()==250);		
+		Assert.assertTrue(aldeano.Libre());		
+	}
+
+
+	@Test
+	public void test02AldeanoReparaPlazaCentral(){		
+		
+		Celda celda = new Celda(15, 15);
+		Jugador jugador1 = new Jugador(100);
+
+		Aldeano aldeano = new Aldeano(celda,jugador1);
+		Mapa.obtenerInstancia().insertar(celda);
+		ArrayList<Zona> zonasPosibles = aldeano.posiblesZonasAConstruirCuartel();
+		Zona zonaAConstruir = zonasPosibles.get(0);
+		aldeano.construirPlazaCentralEnZona(zonaAConstruir);
+
+		aldeano.realizarTareas();
+		aldeano.realizarTareas();
+		aldeano.realizarTareas();
+
+		Assert.assertTrue(aldeano.Libre());
+
+		Edificio plaza = jugador1.obtenerEdificio();
+
+		Assert.assertTrue(plaza.vidaActual()==450);
+		plaza.recibirDanio(70);
+		Assert.assertTrue(plaza.vidaActual()==380);
+		aldeano.repararEdificio(plaza);
+		aldeano.realizarTareas();
+		Assert.assertTrue(plaza.vidaActual()==405);
+		aldeano.realizarTareas();
+		Assert.assertTrue(plaza.vidaActual()==430);		
+		aldeano.realizarTareas();
+		Assert.assertTrue(plaza.vidaActual()==450);		
+		Assert.assertTrue(aldeano.Libre());		
+	}
+	
 
 }
