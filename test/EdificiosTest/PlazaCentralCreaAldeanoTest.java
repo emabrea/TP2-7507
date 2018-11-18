99ac3fb7b239a4dcdaa17760b557ebc25a586e97
Emanuel <42225702+emabrea@users.noreply.github.com>
2018-11-18 15:46:10 -0300

    Add files via upload

--- test/EdificiosTest/PlazaCentralCreaAldeanoTest.java
+++ test/EdificiosTest/PlazaCentralCreaAldeanoTest.java
@@ -4,21 +4,28 @@ import java.util.ArrayList;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 
 import Modelo.Celda;
 import Modelo.Mapa;
 import Modelo.NoSePuedeCrearElAldeanoCeldasPerifericasOcupadasException;
 import Modelo.PlazaCentral;
 import Modelo.Zona;
+import Modelo.Jugador;
 
 public class PlazaCentralCreaAldeanoTest {
 
+	@Before
+	public void reset(){
+		Mapa.reset();
+	}
+
 	@Test
-	public void test01PlazaCentralCreaAldeanoAlLadoSuyoSiEstanVaciasLasCeldas(){
+	public void test01PlazaCentralCreaAldeanoAlLadoSuyoSiEstanVaciasLasCeldas(){		
 		
-		Mapa mapa = new Mapa();
-		int baseMapa = mapa.getTamanioBase();
-		int alturaMapa = mapa.getTamanioAltura();
+		Jugador jugador = new Jugador(100);
+		int baseMapa = Mapa.obtenerInstancia().getTamanioBase();
+		int alturaMapa = Mapa.obtenerInstancia().getTamanioAltura();
 		
 		Celda celda = new Celda(baseMapa - 1, alturaMapa);
 		
@@ -26,36 +33,36 @@ public class PlazaCentralCreaAldeanoTest {
 		int altura = PlazaCentral.getTamanioAltura();
 		Zona zona = new Zona(celda, base, altura);
 		
-		PlazaCentral plazaCentral = new PlazaCentral(celda);
+		PlazaCentral plazaCentral = new PlazaCentral(celda,jugador);
 		
-		mapa.insertar(zona);
+		Mapa.obtenerInstancia().insertar(zona);
 		
-		Assert.assertTrue(mapa.posicionOcupada(zona));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(zona));
 		
 		// Busca las celdas posibles donde puede respawnear el Aldeano
-		ArrayList<Celda> posiblesCeldas = plazaCentral.posiblesCeldasParaCrearAldeano(mapa);
+		ArrayList<Celda> posiblesCeldas = plazaCentral.posiblesCeldasParaCrearAldeano();
 		
 		celda.desplazarIzquierda();
-		Assert.assertFalse(mapa.posicionOcupada(celda));
+		Assert.assertFalse(Mapa.obtenerInstancia().posicionOcupada(celda));
 		
 		// Elijo una (la primera que encontro)
 		Celda posibleCelda = posiblesCeldas.get(0);
 		
-		plazaCentral.crearAldeano(mapa, posibleCelda);
+		plazaCentral.crearAldeano(posibleCelda);
 		
 		// Es justo la celda donde se verifico que antes de crear el Aldeano, la misma estaba libre
 		Assert.assertTrue(celda.igualA(posibleCelda));
 		
-		Assert.assertTrue(mapa.posicionOcupada(zona));
-		Assert.assertTrue(mapa.posicionOcupada(posibleCelda));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(zona));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(posibleCelda));		
 	}
 	
 	@Test
-	public void test02PlazaCentralCreaAldeanoEnLaUnicaCeldaQueNoEstaOcupadaDeLasPerifericas(){
+	public void test02PlazaCentralCreaAldeanoEnLaUnicaCeldaQueNoEstaOcupadaDeLasPerifericas(){		
 		
-		Mapa mapa = new Mapa();
-		int baseMapa = mapa.getTamanioBase();
-		int alturaMapa = mapa.getTamanioAltura();
+		Jugador jugador = new Jugador(100);
+		int baseMapa = Mapa.obtenerInstancia().getTamanioBase();
+		int alturaMapa = Mapa.obtenerInstancia().getTamanioAltura();
 		
 		Celda celdaInicial = new Celda(baseMapa - 1, alturaMapa);
 		Celda celdaDondeIriaElAldeano = new Celda(baseMapa - 2, alturaMapa - 2);
@@ -72,46 +79,46 @@ public class PlazaCentralCreaAldeanoTest {
 		int altura = PlazaCentral.getTamanioAltura();
 		Zona zona = new Zona(celdaInicial, base, altura);
 		
-		PlazaCentral plazaCentral = new PlazaCentral(celdaInicial);
+		PlazaCentral plazaCentral = new PlazaCentral(celdaInicial,jugador);
 		
-		mapa.insertar(zona);
+		Mapa.obtenerInstancia().insertar(zona);
 		// Ocupada la zona de la plaza central una vez insertada
-		Assert.assertTrue(mapa.posicionOcupada(zona));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(zona));
 		
 		for(Celda celda: celdasPeriferia){
-			mapa.insertar(celda);
+			Mapa.obtenerInstancia().insertar(celda);
 			// Celdas de la periferia de la zona ocupadas luego de insertarlas
-			Assert.assertTrue(mapa.posicionOcupada(celda));
+			Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(celda));
 		}
 		
 		// Desocupada la celda donde tendria que ir
-		Assert.assertFalse(mapa.posicionOcupada(celdaDondeIriaElAldeano));
+		Assert.assertFalse(Mapa.obtenerInstancia().posicionOcupada(celdaDondeIriaElAldeano));
 		
 		// Busca las celdas posibles donde puede respawnear el Aldeano
-		ArrayList<Celda> posiblesCeldas = plazaCentral.posiblesCeldasParaCrearAldeano(mapa);
+		ArrayList<Celda> posiblesCeldas = plazaCentral.posiblesCeldasParaCrearAldeano();
 		
 		// Elijo una (es la unica)
 		Assert.assertTrue(posiblesCeldas.size() == 1);
 		Celda posibleCelda = posiblesCeldas.get(0);
 		
-		plazaCentral.crearAldeano(mapa, posibleCelda);
+		plazaCentral.crearAldeano( posibleCelda);
 		
 		// Siguen ocupadas la zona de la plaza central y las celdas de la periferia
-		Assert.assertTrue(mapa.posicionOcupada(zona));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(zona));
 		for(Celda celda: celdasPeriferia){
-			Assert.assertTrue(mapa.posicionOcupada(celda));
+			Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(celda));
 		}
 		// Ahora queda ocupada la celda donde tendria que ir el Aldeano y Es la misma que se obtuvo
 		Assert.assertTrue(celdaDondeIriaElAldeano.igualA(posibleCelda));
-		Assert.assertTrue(mapa.posicionOcupada(posibleCelda));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(posibleCelda));
 	}
 	
 	@Test
-	public void test03PlazaCentralNoCreaAldeanoYaQueSusCeldasPerifericasEstanOcupadas(){
+	public void test03PlazaCentralNoCreaAldeanoYaQueSusCeldasPerifericasEstanOcupadas(){		
 		
-		Mapa mapa = new Mapa();
-		int baseMapa = mapa.getTamanioBase();
-		int alturaMapa = mapa.getTamanioAltura();
+		Jugador jugador = new Jugador(100);
+		int baseMapa = Mapa.obtenerInstancia().getTamanioBase();
+		int alturaMapa = Mapa.obtenerInstancia().getTamanioAltura();
 		
 		Celda celdaInicial = new Celda(baseMapa - 1, alturaMapa);
 		
@@ -127,27 +134,28 @@ public class PlazaCentralCreaAldeanoTest {
 		int altura = PlazaCentral.getTamanioAltura();
 		Zona zona = new Zona(celdaInicial, base, altura);
 		
-		PlazaCentral plazaCentral = new PlazaCentral(celdaInicial);
+		PlazaCentral plazaCentral = new PlazaCentral(celdaInicial,jugador);
 		
-		mapa.insertar(zona);
+		Mapa.obtenerInstancia().insertar(zona);
 		// Ocupada la zona de la plaza central una vez insertada
-		Assert.assertTrue(mapa.posicionOcupada(zona));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(zona));
 		
 		for(Celda celda: celdasPeriferia){
-			mapa.insertar(celda);
+			Mapa.obtenerInstancia().insertar(celda);
 			// Celdas de la periferia de la zona ocupadas luego de insertarlas
-			Assert.assertTrue(mapa.posicionOcupada(celda));
+			Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(celda));
 		}
 		
 		try{
 			// Busca las celdas posibles donde puede respawnear el Aldeano
-			plazaCentral.posiblesCeldasParaCrearAldeano(mapa);
+			plazaCentral.posiblesCeldasParaCrearAldeano();
 		} catch(NoSePuedeCrearElAldeanoCeldasPerifericasOcupadasException e){ }
 		
 		// Siguen ocupadas la zona de la Plaza Central y las celdas de la periferia
-		Assert.assertTrue(mapa.posicionOcupada(zona));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(zona));
 		for(Celda celda: celdasPeriferia){
-			Assert.assertTrue(mapa.posicionOcupada(celda));
+			Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(celda));
 		}
 	}
+	
 }
