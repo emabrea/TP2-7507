904b7d2cdaa1c8276417eebf9c23ec4e94225e4a
Emanuel <42225702+emabrea@users.noreply.github.com>
2018-11-18 15:47:28 -0300

    Add files via upload

--- test/UnidadesTest/AldeanoConstruyeTest.java
+++ test/UnidadesTest/AldeanoConstruyeTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 
 import Modelo.Aldeano;
 import Modelo.Celda;
@@ -12,240 +13,229 @@ import Modelo.Zona;
 import Modelo.Jugador;
 
 public class AldeanoConstruyeTest {
+
+	@Before
+	public void reset(){
+		Mapa.reset();
+	}
 	
 	@Test
-	public void test01AldeanoConstruyeCuartelLoConstruyeEnZonaASuLado(){
+	public void test01AldeanoConstruyeCuartelLoConstruyeEnZonaASuLado(){		
 		
-		Mapa mapa = new Mapa();
-		Celda celda = new Celda(0, 0);
+		Celda celda = new Celda(15, 15);
 		Jugador jugador1 = new Jugador(100);
 
-		Aldeano aldeano = new Aldeano(celda);
+		Aldeano aldeano = new Aldeano(celda,jugador1);
 		Assert.assertTrue(aldeano.Libre());
 		
-		mapa.insertar(celda);	
-		Assert.assertTrue(mapa.posicionOcupada(celda));
+		Mapa.obtenerInstancia().insertar(celda);	
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(celda));
 		
 		// Retorna una zona de posibles posiciones para construir
-		ArrayList<Zona> zonasPosibles = aldeano.posiblesZonasAConstruirCuartel(mapa);
-		Assert.assertFalse(aldeano.Libre());
+		ArrayList<Zona> zonasPosibles = aldeano.posiblesZonasAConstruirCuartel();
+		Assert.assertTrue(aldeano.Libre());
 		
 		Zona zonaAConstruir = zonasPosibles.get(0);
-		aldeano.construirCuartelEnZona(zonaAConstruir, mapa);
+		aldeano.construirCuartelEnZona(zonaAConstruir);
 		Assert.assertFalse(aldeano.Libre());
 
 		//turno 1
-		aldeano.realizarTareas(jugador1);
+		aldeano.realizarTareas();
 		Assert.assertFalse(aldeano.Libre());
 
 		//turno 2
-		aldeano.realizarTareas(jugador1);
+		aldeano.realizarTareas();
 		Assert.assertFalse(aldeano.Libre());
 
 		//turno 3
-		aldeano.realizarTareas(jugador1);
+		aldeano.realizarTareas();
 		Assert.assertTrue(aldeano.Libre());
 		
-		Assert.assertTrue(mapa.posicionOcupada(celda));
-		Assert.assertTrue(mapa.posicionOcupada(zonaAConstruir));
-	}
-	
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(celda));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(zonaAConstruir)); 
+	}	
+
 	@Test
-	public void test02AldeanoConstruyePlazaCentralLoConstruyeEnZonaASuLado(){
+	public void test02AldeanoConstruyePlazaCentralLoConstruyeEnZonaASuLado(){		
 		
-		Mapa mapa = new Mapa();
-		Celda celda = new Celda(0, 0);
+		Celda celda = new Celda(15, 15);
 		Jugador jugador1 = new Jugador(100);
 
 
-		Aldeano aldeano = new Aldeano(celda);
+		Aldeano aldeano = new Aldeano(celda,jugador1);
 		Assert.assertTrue(aldeano.Libre());
 		
-		mapa.insertar(celda);	
-		Assert.assertTrue(mapa.posicionOcupada(celda));	
+		Mapa.obtenerInstancia().insertar(celda);	
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(celda));	
 		
 		
-		ArrayList<Zona> zonasPosibles = aldeano.posiblesZonasAConstruirPlazaCentral(mapa);		
+		ArrayList<Zona> zonasPosibles = aldeano.posiblesZonasAConstruirPlazaCentral();		
 		Zona zonaAConstruir = zonasPosibles.get(0);
 
-		aldeano.construirPlazaCentralEnZona(zonaAConstruir, mapa);
+		aldeano.construirPlazaCentralEnZona(zonaAConstruir);
 		Assert.assertFalse(aldeano.Libre());
 
 		//turno 1
-		aldeano.realizarTareas(jugador1);
+		aldeano.realizarTareas();
 		Assert.assertFalse(aldeano.Libre());
 
 		//turno 2
-		aldeano.realizarTareas(jugador1);
+		aldeano.realizarTareas();
 		Assert.assertFalse(aldeano.Libre());
 
 		//turno 3
-		aldeano.realizarTareas(jugador1);
+		aldeano.realizarTareas();
 		Assert.assertTrue(aldeano.Libre());
 		
-		Assert.assertTrue(mapa.posicionOcupada(celda));
-		Assert.assertTrue(mapa.posicionOcupada(zonaAConstruir));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(celda));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(zonaAConstruir));
 	}
-
+	
 	@Test
-	public void test03AldeanoConstruyeEnTurnoCorrecto(){
+	public void test03AldeanoConstruyeEnTurnoCorrecto(){		
 		
-		Mapa mapa = new Mapa();
-		Celda celda1 = new Celda(0, 0);
+		Celda celda1 = new Celda(20, 20);
 		Celda celda2 = new Celda(10, 10);
 
 		Jugador jugador1 = new Jugador(100);
 		Jugador jugador2 = new Jugador(100);
 
-		Aldeano aldeano1 = new Aldeano(celda1);
-		Aldeano aldeano2 = new Aldeano(celda2);	
-		mapa.insertar(celda1);	
-		mapa.insertar(celda2);	
+		Aldeano aldeano1 = new Aldeano(celda1,jugador1);
+		Aldeano aldeano2 = new Aldeano(celda2,jugador2);	
+		Mapa.obtenerInstancia().insertar(celda1);	
+		Mapa.obtenerInstancia().insertar(celda2);	
 		
 		
-		ArrayList<Zona> zonasPosibles = aldeano1.posiblesZonasAConstruirPlazaCentral(mapa);		
+		ArrayList<Zona> zonasPosibles = aldeano1.posiblesZonasAConstruirPlazaCentral();		
 		Zona zonaAConstruir = zonasPosibles.get(0);
 
-		aldeano1.construirPlazaCentralEnZona(zonaAConstruir, mapa);
+		aldeano1.construirPlazaCentralEnZona(zonaAConstruir);
 
 		Assert.assertFalse(aldeano1.Libre());
 		Assert.assertTrue(aldeano2.Libre());
 
 		//turno 1 jugador 1
-		aldeano1.realizarTareas(jugador1);
+		aldeano1.realizarTareas();
 		Assert.assertFalse(aldeano1.Libre());
 		Assert.assertTrue(aldeano2.Libre());
 
 		//turno 2 jugador 1
-		aldeano1.realizarTareas(jugador1);
+		aldeano1.realizarTareas();
 		Assert.assertFalse(aldeano1.Libre());
 		Assert.assertTrue(aldeano2.Libre());
 
 		//turno 3 jugador 1
-		aldeano1.realizarTareas(jugador1);
+		aldeano1.realizarTareas();
 		Assert.assertTrue(aldeano1.Libre());
 		Assert.assertTrue(aldeano2.Libre());
 
 
-		ArrayList<Zona> zonasPosibles2 = aldeano2.posiblesZonasAConstruirPlazaCentral(mapa);		
+		ArrayList<Zona> zonasPosibles2 = aldeano2.posiblesZonasAConstruirPlazaCentral();		
 		Zona zonaAConstruir2 = zonasPosibles2.get(0);
 
-		aldeano2.construirPlazaCentralEnZona(zonaAConstruir2, mapa);
+		aldeano2.construirPlazaCentralEnZona(zonaAConstruir2);
 
 		//turno 1 jugador 2
-		aldeano2.realizarTareas(jugador2);
+		aldeano2.realizarTareas();
 		Assert.assertTrue(aldeano1.Libre());
 		Assert.assertFalse(aldeano2.Libre());
 		
-		Assert.assertTrue(mapa.posicionOcupada(celda1));
-		Assert.assertTrue(mapa.posicionOcupada(celda2));
-		Assert.assertTrue(mapa.posicionOcupada(zonaAConstruir));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(celda1));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(celda2));
+		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(zonaAConstruir));
 	}
 
-
+	
 	@Test
-	public void test04AldeanoConstruyeYElOroNoAumenta(){
+	public void test04AldeanoConstruyeYElOroNoAumenta(){		
 		
-		Mapa mapa = new Mapa();
-		Celda celda = new Celda(0, 0);
+		Celda celda = new Celda(15, 15);
 		Jugador jugador = new Jugador(100);
 
-		Aldeano aldeano = new Aldeano(celda);
+		Aldeano aldeano = new Aldeano(celda,jugador);
 		Assert.assertTrue(aldeano.Libre());
 
-		mapa.insertar(celda);		
-		ArrayList<Zona> zonasPosibles = aldeano.posiblesZonasAConstruirPlazaCentral(mapa);		
+		Mapa.obtenerInstancia().insertar(celda);		
+		ArrayList<Zona> zonasPosibles = aldeano.posiblesZonasAConstruirPlazaCentral();		
 		Zona zonaAConstruir = zonasPosibles.get(0);
-		aldeano.construirPlazaCentralEnZona(zonaAConstruir, mapa);
+		aldeano.construirPlazaCentralEnZona(zonaAConstruir);
 		
 		Assert.assertTrue(jugador.cantidadDeOro()==100);
 
 		//turno 1
-		aldeano.realizarTareas(jugador);
+		aldeano.realizarTareas();
 		Assert.assertFalse(aldeano.Libre());
 
 		Assert.assertTrue(jugador.cantidadDeOro()==100);
 		
 	}
-
+	
 	@Test
-	public void test05AldeanoNoConstruyeYElOroAumenta(){
-		
-		Mapa mapa = new Mapa();
-		Celda celda = new Celda(0, 0);
+	public void test05AldeanoNoConstruyeYElOroAumenta(){		
+	
+		Celda celda = new Celda(20, 20);
 		Jugador jugador = new Jugador(100);
-		mapa.insertar(celda);
+		Mapa.obtenerInstancia().insertar(celda);
 
-		Aldeano aldeano = new Aldeano(celda);
+		Aldeano aldeano = new Aldeano(celda,jugador);
 		Assert.assertTrue(aldeano.Libre());		
 		
 		Assert.assertTrue(jugador.cantidadDeOro()==100);
 
 		//turno 1
-		aldeano.realizarTareas(jugador);
-		Assert.assertFalse(aldeano.Libre());//esta recolectando oro
+		aldeano.realizarTareas();
+		Assert.assertTrue(aldeano.Libre());//esta recolectando oro
 
 		Assert.assertFalse(jugador.cantidadDeOro()==100);
 		Assert.assertTrue(jugador.cantidadDeOro()==120);
 
 		//turno 2
-		aldeano.realizarTareas(jugador);
+		aldeano.realizarTareas();
 		Assert.assertTrue(jugador.cantidadDeOro()==140);
 
 
-		ArrayList<Zona> zonasPosibles = aldeano.posiblesZonasAConstruirPlazaCentral(mapa);		
+		ArrayList<Zona> zonasPosibles = aldeano.posiblesZonasAConstruirPlazaCentral();		
 		Zona zonaAConstruir = zonasPosibles.get(0);
-		aldeano.construirPlazaCentralEnZona(zonaAConstruir, mapa);
+		aldeano.construirPlazaCentralEnZona(zonaAConstruir);
 
 		//turno 3
-		aldeano.realizarTareas(jugador);
+		aldeano.realizarTareas();
 		Assert.assertTrue(jugador.cantidadDeOro()==140);
 		
 	}
-
+	
 	@Test
-	public void test06AumentaElOroLuegoDeConstruir(){
+	public void test06AumentaElOroLuegoDeConstruir(){		
 		
-		Mapa mapa = new Mapa();
-		Celda celda = new Celda(0, 0);
+		Celda celda = new Celda(20, 20);
 		Jugador jugador = new Jugador(100);
-		mapa.insertar(celda);
+		Mapa.obtenerInstancia().insertar(celda);
 
-		Aldeano aldeano = new Aldeano(celda);	
+		Aldeano aldeano = new Aldeano(celda,jugador);	
 
-		ArrayList<Zona> zonasPosibles = aldeano.posiblesZonasAConstruirPlazaCentral(mapa);		
+		ArrayList<Zona> zonasPosibles = aldeano.posiblesZonasAConstruirPlazaCentral();		
 		Zona zonaAConstruir = zonasPosibles.get(0);
-		aldeano.construirPlazaCentralEnZona(zonaAConstruir, mapa);			
+		aldeano.construirPlazaCentralEnZona(zonaAConstruir );			
 
 		//turno 1
-		aldeano.realizarTareas(jugador);
+		aldeano.realizarTareas();
 		Assert.assertFalse(aldeano.Libre());
 		Assert.assertTrue(jugador.cantidadDeOro()==100);
 
 		//turno 2
-		aldeano.realizarTareas(jugador);
+		aldeano.realizarTareas();
 		Assert.assertFalse(aldeano.Libre());
 		Assert.assertTrue(jugador.cantidadDeOro()==100);
 
 		//turno 3
-		aldeano.realizarTareas(jugador);
+		aldeano.realizarTareas();
 		Assert.assertTrue(aldeano.Libre());
 		Assert.assertTrue(jugador.cantidadDeOro()==100);
 
 		//turno 4
-		aldeano.realizarTareas(jugador);		
-		Assert.assertTrue(jugador.cantidadDeOro()==120);
+		aldeano.realizarTareas();		
+		Assert.assertTrue(jugador.cantidadDeOro()==120);		
 		
-		
-		
-	}
+	}	
 	
-
-
-
-
-
-
-	
-		
 }
