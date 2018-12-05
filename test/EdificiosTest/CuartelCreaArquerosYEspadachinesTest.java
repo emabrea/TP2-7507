8242240e85a04fd71433e571e0f0203dafe634e5
Emanuel <42225702+emabrea@users.noreply.github.com>
2018-12-05 00:11:53 -0300

    descomento pruebas

--- test/EdificiosTest/CuartelCreaArquerosYEspadachinesTest.java
+++ test/EdificiosTest/CuartelCreaArquerosYEspadachinesTest.java
@@ -134,11 +134,11 @@ public class CuartelCreaArquerosYEspadachinesTest {
 			Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(celda));
 		}
 
-		/*
+		
 		try{
 			// Busca las celdas posibles donde puede respawnear el Arquero
 			cuartel.posiblesCeldasParaCrearArquero();
-		} catch(NoSePuedeCrearElArqueroCeldasPerifericasOcupadasException e){ }*/
+		} catch(NoSePuedeCrearElArqueroCeldasPerifericasOcupadasException e){ }
 		
 		// Siguen ocupadas la zona del cuartel y las celdas de la periferia
 		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(zona));
@@ -280,11 +280,11 @@ public class CuartelCreaArquerosYEspadachinesTest {
 			Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(celda));
 		}
 		
-		/*
+		
 		try{
 			// Busca las celdas posibles donde puede respawnear el Espadachin
 			castillo.posiblesCeldasParaCrearEspadachin();
-		} catch(NoSePuedeCrearElEspadachinCeldasPerifericasOcupadasException e){ }*/
+		} catch(NoSePuedeCrearElEspadachinCeldasPerifericasOcupadasException e){ }
 		
 		// Siguen ocupadas la zona del cuartel y las celdas de la periferia
 		Assert.assertTrue(Mapa.obtenerInstancia().posicionOcupada(zona));
