771ecc1737138ee2df424122f3a8b48dc54c3a63
Francisco Jose Nasif <44707059+franjon11@users.noreply.github.com>
2018-12-05 11:57:59 -0300

    Add files via upload

---
+++ src/Controladores/BotonJugarEventHandler.java
@@ -0,0 +1,42 @@
+package Controladores;
+
+import Vista.ContenedorJugador;
+import Vista.ContenedorPrincipal;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class BotonJugarEventHandler implements EventHandler<ActionEvent>{
+
+	TextField nombreJugador1;
+	TextField nombreJugador2;
+	Stage stage;
+
+	public BotonJugarEventHandler(Stage stage, TextField nombreJugador1, TextField nombreJugador2){
+		this.stage = stage;
+		this.nombreJugador1 = nombreJugador1;
+		this.nombreJugador2 = nombreJugador2;
+	}
+
+	@Override
+	public void handle(ActionEvent event) {
+
+		Juego juego = new Juego(nombreJugador1.getText(), nombreJugador2.getText());
+        HBox root = new HBox(5);
+        ControladorDeTurno.getInstance().asignarJugadores(juego.obtenerJugadores());
+        ContenedorPrincipal contenedorPrincipal = new ContenedorPrincipal(juego, root);
+        ControladorDeTurno.getInstance().setearContenedor(contenedorPrincipal);
+
+        Scene scene = new Scene(root, 500, 500);
+        stage.setTitle("Algo of Empires");
+        stage.setHeight(700);
+        stage.setWidth(900);
+        stage.setScene(scene);
+	}
+
+
+}
